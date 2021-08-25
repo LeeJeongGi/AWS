39588d3cc9bbb4154a637513dd6ea5500cb7a522
LeeJeongGi <apple4rhk@naver.com>
2021-08-25 23:12:43 +0900

    룸북을 이용한 게시판 CRUD 만들기 시작_

---
+++ src/main/java/springboot/domain/posts/JpaRepository.java
@@ -0,0 +1,6 @@
+package springboot.domain.posts;
+
+public interface JpaRepository  extends org.springframework.data.jpa.repository.JpaRepository<Posts, Long> {
+
+    //JpaRepository<Posts, Long>를 상속하면 기본적인 Crud 메소드가 자동으로 생성된다.
+}
---
+++ src/main/java/springboot/domain/posts/Posts.java
@@ -0,0 +1,32 @@
+package springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter //클래사 내 모든 필드의 Getter 메소드를 자동생성
+@NoArgsConstructor //기본 생성자
+@Entity //테이블과 링크될 클래스임을 나타낸다
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //pk 생성규칙, 이옵션을 추가해야만 auto_increment가 된다
+    private Long id;
+
+    @Column(length = 500, nullable = false) //Column 안써도 인식되지만 설정값을 적어주기 위해 사용
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author)
+    {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
