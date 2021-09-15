fd8b5599475c5f6aa9efccda9e6724a7fc6ac1d0
LeeJeongGi <apple4rhk@naver.com>
2021-09-15 22:34:46 +0900

    4 게시판 CRUD 기능 모두 구현완료

--- src/main/java/springboot/domain/posts/PostsRepository.java
+++ src/main/java/springboot/domain/posts/PostsRepository.java
@@ -1,6 +1,13 @@
 package springboot.domain.posts;
 
-public interface PostsRepository extends org.springframework.data.jpa.repository.JpaRepository<Posts, Long> {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-    //JpaRepository<Posts, Long>를 상속하면 기본적인 Crud 메소드가 자동으로 생성된다.
+import java.util.List;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
+
 }
